1fba1ced3a1b825ab15c95179e1183a130543f82
VIP <azrino@ittelkom-pwt.ac.id>
2022-04-18 23:10:45 +0700

    Iniit Praktikum 6

--- TESTING/My Application/praktikum6/src/main/java/com/example/praktikum6/MainActivity.kt
+++ TESTING/My Application/praktikum6/src/main/java/com/example/praktikum6/MainActivity.kt
@@ -2,10 +2,78 @@ package com.example.praktikum6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.praktikum6.adapter.GridMyDataAdapter
+import com.example.praktikum6.adapter.ListMyDataAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val list = ArrayList<MyData>()
     override fun onCreate(savedInstanceState: Bundle?) {
+        rv_mydata.setHasFixedSize(true)
+        list.addAll(getListMyDatas())
+        showRecyclerList()
+        
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fun getListMyDatas():
+                ArrayList<MyData> {
+            val dataName = resources.getStringArray(R.array.data_name)
+            val dataDescription = resources.getStringArray(R.array.data_description)
+            val dataPhoto = resources.getStringArray(R.array.data_photo)
+            val listMyData = ArrayList<MyData>()
+            for (position in dataName.indices) {
+                val myData = MyData(
+                    dataName[position],
+                    dataDescription[position],
+                    dataPhoto[position]
+                )
+                listMyData.add(myData)
+            }
+            return listMyData
+        }
     }
+
+    private fun getListMyDatas(): Collection<MyData> {
+
+    }
+
+    private fun showRecyclerList() {
+        rv_mydata.layoutManager = LinearLayoutManager(this)
+        val listMyDataAdapter = ListMyDataAdapter(list)
+        rv_mydata.adapter = listMyDataAdapter
+    }
+    private fun showRecyclerGrid() {
+        rv_mydata.layoutManager = GridLayoutManager(this, 2)
+        val gridMyDataAdapter = GridMyDataAdapter(list)
+        rv_mydata.adapter = gridMyDataAdapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId) return super.onOptionsItemSelected(item)
+    }
+    private fun setMode(selectedMode: Int) {
+        when (selectedMode) {
+            R.id.action_list -> {
+                showRecyclerList()
+            }
+            R.id.action_grid -> {
+                showRecyclerGrid()
+            }
+            R.id.action_cardview -> {
+            }
+        }
+    }
+
+
+    }
+
 }
--- TESTING/My Application/praktikum6/src/main/java/com/example/praktikum6/MyData.kt
+++ TESTING/My Application/praktikum6/src/main/java/com/example/praktikum6/MyData.kt
@@ -3,7 +3,17 @@ package com.example.praktikum6
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
-class MyData {
+class MyData(s: String, s1: String, s2: String) {
+    val description: CharSequence? = null
+    val name: CharSequence?
+        get() {
+            TODO()
+        }
+    val photo: Any
+        get() {
+            TODO()
+        }
+
     @Parcelize
     data class MyData(
         var name: String,
